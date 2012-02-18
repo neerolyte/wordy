56d15bcc4526b67355814eac3112d6a1e95f0658
David Schoen <dave@lyte.id.au>
2012-02-19 08:58:30 +1100

    basics functioning under node

--- wordy.js
+++ wordy.js
@@ -1,28 +1,10 @@
-(function(exports){
-	var letterMatch = function(letters, word) {
-		for (var l in letters) {
-			var letter = letters[l];
-			var i = word.indexOf(letter);
-			if (i == -1)
-				return false;
-			// remove matched letter from word
-			word = word.slice(0, i) + word.slice(i+1);
-		}
-		return true;
-	};
-	
-	var getWords = function(letters) {
-		var matches = [];
-		
-		for (var w in words) {
-			var word = words[w];
-			if (letterMatch(letters, word))
-				matches.push(word)
-		}
-		
-		return matches;
-	};
-	exports.getWords = getWords;
-})(typeof exports === 'undefined' ? this['wordy']={} : exports);
+#!/usr/bin/env node
+var wordy = require('./lib/wordy');
 
-	
+var args = process.argv.slice(2);
+
+wordy.setWords(['ball', 'bat']);
+
+for (var a in args) {
+	console.log(wordy.getWords(args[a]));
+}
