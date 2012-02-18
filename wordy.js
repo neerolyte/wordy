af89c1cf8db4cc8b686bbf55583fe890458c9318
David Schoen <dave@lyte.id.au>
2012-02-18 16:36:04 +1100

    working in browser with closure

--- wordy.js
+++ wordy.js
@@ -1,25 +1,28 @@
-function letterMatch(letters, word) {
-	for (var l in letters) {
-		var letter = letters[l];
-		var i = word.indexOf(letter);
-		if (i == -1)
-			return false;
-		// remove matched letter from word
-		word = word.slice(0, i) + word.slice(i+1);
-	}
-	return true;
-}
-
-function getWords(letters) {
-	var matches = [];
-
-	for (var w in words) {
-		var word = words[w];
-		if (letterMatch(letters, word))
-			matches.push(word)
-	}
+(function(exports){
+	var letterMatch = function(letters, word) {
+		for (var l in letters) {
+			var letter = letters[l];
+			var i = word.indexOf(letter);
+			if (i == -1)
+				return false;
+			// remove matched letter from word
+			word = word.slice(0, i) + word.slice(i+1);
+		}
+		return true;
+	};
 	
-	return matches;
-}
+	var getWords = function(letters) {
+		var matches = [];
+		
+		for (var w in words) {
+			var word = words[w];
+			if (letterMatch(letters, word))
+				matches.push(word)
+		}
+		
+		return matches;
+	};
+	exports.getWords = getWords;
+})(typeof exports === 'undefined' ? this['wordy']={} : exports);
 
-console.log(getWords('BALLZUIO'));
+	
