e445f69f259c8721add997ac29be5303708158d2
David Schoen <dave@lyte.id.au>
2012-03-23 15:57:41 +1100

    let user choose filter type

--- browser.js
+++ browser.js
@@ -1,17 +1,25 @@
 var w;
 var listReq = $.ajax('lists/enable2k.txt');
 listReq.done(function(data) {
-	var words = data.split('\n');
-	// clean up trailing empty elements
-	while (words[words.length-1] == '') {
-		words.pop();
-	}
-	w = wordy.create(words);
+	w = wordy.create(data);
 });
 
 /**
  * Update the display based on the current filters
  */
 function update() {
-	$('#result').html(w.anagrams(filters.filter0.value).sort().join(','));
+	var filters = $('.filters .filter');
+
+	var filter = filters[0];
+	
+	var str = $(filter).find('.string')[0].value;
+	var type = $(filter).find('.type')[0].value;
+	
+	$('#result').html(w[type](str).sort().join(','));
 }
+
+
+$(document).ready(function() {
+	// focus on top input at page load
+	$(".filters .filter .string")[0].focus();
+});
