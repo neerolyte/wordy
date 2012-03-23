28f73b152afc3730014b2e32172b02796fa68f9c
David Schoen <dave@lyte.id.au>
2012-03-23 17:11:37 +1100

    got filters stacking in browser

--- browser.js
+++ browser.js
@@ -5,19 +5,52 @@ listReq.done(function(data) {
 });
 
 /**
- * Update the display based on the current filters
+ * Update the results div based on the current filters
  */
 function update() {
 	var filters = $('.filters .filter');
 
-	var filter = filters[0];
+	var res = w;
+	// loop over ze filters!
+	for (var i = 0; i < filters.length; i++) {
+		var filter = filters[i];
 	
-	var str = $(filter).find('.string')[0].value;
-	var type = $(filter).find('.type')[0].value;
+		var str = $(filter).find('.string')[0].value;
+		var type = $(filter).find('.type')[0].value;
+
+		// bail if str is not set
+		if (str == '') break;
+
+		res = res[type](str);
+	}
+
+	res = res.sort();
 	
-	$('#result').html(w[type](str).sort().join(','));
+	$('#result').html(res.join(', '));
+	$('#resultcount').html(res.length);
+
+	expand();
 }
 
+/**
+ * Checks whether we should expand the filters area
+ * because all the current ones may be in use
+ */
+function expand() {
+	var filters = $('.filters li.filter');
+	var lastFilter = filters[filters.length - 1];
+
+	var lastFilterStr = $(lastFilter).find('.string')[0].value;
+	if (lastFilterStr != '') {
+		console.log("would expand");
+		var newFilter = $(lastFilter).clone(true)
+
+		// reset input
+		newFilter.find('input.string')[0].value = '';
+		
+		newFilter.appendTo('.filters ul');
+	}
+}
 
 $(document).ready(function() {
 	// focus on top input at page load
